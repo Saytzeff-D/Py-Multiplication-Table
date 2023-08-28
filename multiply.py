4515b894eb956c52c5e19a42427b175569febaa8
David Atanda <ololadedavid15@gmail.com>
2023-08-28 11:00:01 +0100

    comment

--- multiply.py
+++ multiply.py
@@ -1,8 +1,8 @@
 val = input('Enter a number: ')
 
-# for x in range(1, int(val)):
-#     for y in range(1, 13):
-#         print(f"{x} x {y} = {int(x) * y}")
+for x in range(1, int(val)):
+    for y in range(1, 13):
+        print(f"{x} x {y} = {int(x) * y}")
     
-for x in range(1, 13):
-    print(f"{val} x {x} = {int(val) * x}")
+# for x in range(1, 13):
+#     print(f"{val} x {x} = {int(val) * x}")
